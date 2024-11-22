fca3af11f753ca6c7dcab6b7a005d1d25c492087
Patipan Buranangura <patipan.b7@gmail.com>
2024-11-22 19:35:57 +0700

    fix build command

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-GOARCH=arm64 GOOS=linux go build -o app *.go
+GOARCH=arm64 GOOS=linux go build -o hcloud-alias-ip *.go
 
 # Tar the executable, service, and install script
-mkdir -p hcloud-alias-ip
-cp app hcloud-alias-ip/
+# mkdir -p hcloud-alias-ip
+# cp hcloud-alias-ip hcloud-alias-ip/
 
-tar -czvf hcloud-alias-ip.arm64.linux.tar.gz hcloud-alias-ip
-rm -rf hcloud-alias-ip
+# tar -czvf hcloud-alias-ip.arm64.linux.tar.gz hcloud-alias-ip
+# rm -rf hcloud-alias-ip
